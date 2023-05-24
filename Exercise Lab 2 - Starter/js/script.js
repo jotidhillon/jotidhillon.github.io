2c7e66d70c5189581a6341b1b8338921cbc8f16d
jotidhillon <11143518+jotidhillon@users.noreply.github.com>
2023-05-23 18:11:28 -0700

    Assignment 1

---
+++ Exercise Lab 2 - Starter/js/script.js
@@ -0,0 +1,61 @@
+//Joti Dhillon
+//CSIS 3380 - 002 Full Stack Web Development with Javascript
+//300348539
+
+//First function fetches data out of the data out of the randomuser api
+function fetchData() {
+fetch("https://randomuser.me/api/?results=53").then((apidata)=>{
+    
+    return apidata.json();
+}).then((newData)=>{
+    
+    let data ="";
+  
+    newData.results.map((values)=>{
+       
+        data+=`<ul class="contact-list">
+        <li class="contact-item cf">
+        <div class="contact-details">
+              <img class="avatar" src="${values.picture.thumbnail}">
+              <h3>${values.name.first} ${values.name.last}</h3>
+              <span class="email">${values.email}</span>
+          </div>
+          <div class="joined-details">
+                 <span class="date">${values.registered.date}</span>
+         </div>
+         </li>
+         </ul>`
+         
+});
+            //shows the contact details for the student
+            document.getElementById("contact").innerHTML=data;
+            totalCount(data);
+            Pages(data);
+    
+}).catch((err)=>{
+    console.log(err);
+});
+
+}
+
+
+/*
+function that should be counting the number for the count per page (10) which would
+calculate the numOfPages with Math ceiling to get the data.length divided by numCountPerPage
+*/
+function Pages(data){
+    let numCountPerPage = 10;
+    let currentPage = 1;
+    let numOfPages = Math.ceil(data.length / numCountPerPage);
+    for(let i=1; i<=numOfPages; i++)  {
+        let startingPoint = (currentPage - 1)*numCountPerPage;
+        let endingPoint = startingPoint + numCountPerPage;
+    }
+}
+
+//function shows the amount of students in total
+function totalCount(data) {
+    let students = 53;
+    document.getElementById("number").innerHTML= "<b>Total: </b>" + students;
+}
+fetchData();
